aad8a0332d2066d206fe5804bc18c79320c459f2
Kumar Vedant <kvedant1024@gmail.com>
2025-01-04 14:49:32 +0530

    Added nav and footer to all, also adjust fonts of events, gallery and all separately

--- Donors/donors.js
+++ Donors/donors.js
@@ -1,8 +1,11 @@
 const menuBtn = document.querySelector(".menu-btn");
 const sidebar = document.querySelector(".sidebar");
 
+const body = document.body;
+
 menuBtn.addEventListener("click", () => {
   sidebar.classList.toggle("active");
+  body.classList.toggle("no-scroll");
 });
 
 function scrollToTop() {
